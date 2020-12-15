7dec0b0f57cf10bdd0d99b15dbf8d9d48d9ff942
Gisela Torres <giselat@microsoft.com>
2020-12-15 16:06:07 +0100

    disabled timeout and generated a new output

--- Lighthouse-Analysis/index.js
+++ Lighthouse-Analysis/index.js
@@ -2,27 +2,39 @@ const lighthouse = require('lighthouse');
 const puppeteer = require('puppeteer');
 
 module.exports = async function(context, req) {
-	const url = req.query.url || (req.body && req.body.url);
-	context.log(`Analyzing URL ${url}`);
+    const url = req.query.url || (req.body && req.body.url);
+    context.log(`Analyzing URL ${url}`);
 
-	// Use Puppeteer
-	const browser = await puppeteer.launch({ headless: true });
+    // Use Puppeteer
+    //https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerlaunchoptions
+    const browser = await puppeteer.launch({ headless: true, timeout: 0 });
 
-	// Lighthouse will analyze the URL using puppeter
-	const { lhr } = await lighthouse(url, {
-		disableDeviceEmulation: true,
-		port: new URL(browser.wsEndpoint()).port,
-		output: 'json',
-		logLevel: 'info'
-	});
 
-	context.log(`Lighthouse scores: ${Object.values(lhr.categories).map((c) => c.score).join(', ')}`);
+    // Lighthouse will analyze the URL using puppeter
+    const { lhr } = await lighthouse(url, {
+        maxWaitForFcp: 30 * 1000,
+        disableDeviceEmulation: true,
+        port: new URL(browser.wsEndpoint()).port,
+        output: 'json',
+        logLevel: 'info'
+    });
 
-	browser.disconnect();
-	browser.close();
+    let result = {
+        performance: lhr.categories.performance.score,
+        accessibility: lhr.categories.accessibility.score,
+        seo: lhr.categories.seo.score,
+        pwa: lhr.categories.pwa.score,
+        bestpractices: lhr.categories["best-practices"].score
+    };
 
-	context.res = {
-		// status: 200, /* Defaults to 200 */
-		body: lhr
-	};
+    context.log(`result for ${url} is ${JSON.stringify(result)}`);
+
+    browser.disconnect();
+    await browser.close();
+
+
+    context.res = {
+        status: result.performance != null ? 200 : 500,
+        body: result
+    };
 };
