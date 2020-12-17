d4aa4fc8e82a8c6ef11ec3a9d96e4859b7a50b54
0GiS0 <gisela.torres@returngis.net>
2020-12-17 11:45:31 +0100

    updated

--- Lighthouse-Analysis/index.js
+++ Lighthouse-Analysis/index.js
@@ -2,39 +2,42 @@ const lighthouse = require('lighthouse');
 const puppeteer = require('puppeteer');
 
 module.exports = async function(context, req) {
-    const url = req.query.url || (req.body && req.body.url);
-    context.log(`Analyzing URL ${url}`);
+	const url = req.query.url || (req.body && req.body.url);
+	context.log(`Analyzing URL ${url}`);
 
-    // Use Puppeteer
-    //https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerlaunchoptions
-    const browser = await puppeteer.launch({ headless: true, timeout: 0 });
+	// Use Puppeteer
+	//https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerlaunchoptions
+	const browser = await puppeteer.launch({
+		headless: true,
+		timeout: 0,
+		args: [ '--no-sandbox' ]		
+	});
 
+	// Lighthouse will analyze the URL using puppeter
+	const { lhr } = await lighthouse(url, {
+		maxWaitForFcp: 30 * 1000,
+		disableDeviceEmulation: true,
+		port: new URL(browser.wsEndpoint()).port,
+		output: 'json',
+		logLevel: 'info',
+		onlyCategories: [ 'seo', 'performance', 'accessibility', 'best-practices' ] //just analyze what you need
+	});
 
-    // Lighthouse will analyze the URL using puppeter
-    const { lhr } = await lighthouse(url, {
-        maxWaitForFcp: 30 * 1000,
-        disableDeviceEmulation: true,
-        port: new URL(browser.wsEndpoint()).port,
-        output: 'json',
-        logLevel: 'info'
-    });
+	let result = {
+		performance: lhr.categories.performance.score,
+		accessibility: lhr.categories.accessibility.score,
+		seo: lhr.categories.seo.score,
+		// pwa: lhr.categories.pwa.score,
+		bestpractices: lhr.categories['best-practices'].score
+	};
 
-    let result = {
-        performance: lhr.categories.performance.score,
-        accessibility: lhr.categories.accessibility.score,
-        seo: lhr.categories.seo.score,
-        pwa: lhr.categories.pwa.score,
-        bestpractices: lhr.categories["best-practices"].score
-    };
+	context.log(`result for ${url} is ${JSON.stringify(result)}`);
 
-    context.log(`result for ${url} is ${JSON.stringify(result)}`);
+	browser.disconnect();
+	await browser.close();
 
-    browser.disconnect();
-    await browser.close();
-
-
-    context.res = {
-        status: result.performance != null ? 200 : 500,
-        body: result
-    };
+	context.res = {
+		status: result.performance != null ? 200 : 500,
+		body: result
+	};
 };
